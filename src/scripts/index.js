803c7d7cec2a5777bd1a1524d586b852331deb6b
soffanderson <s.btudz@gmail.com>
2023-05-11 22:07:23 +0800

    Mengubah kode keras manjadi dom manipulation JSON.

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,12 +1,38 @@
 import 'regenerator-runtime'; /* for async await transpile */
 import '../styles/main.scss';
-
-console.log('Hello Coders! :)');
+import data from '../DATA.json';
 
 const hero = document.querySelector('.hero');
 const main = document.querySelector('main');
 const drawer = document.querySelector('#drawer');
 const checkbox = document.querySelector('#openMenu');
+
+let restaurantElement = '';
+data.restaurants.forEach((resto) => {
+  restaurantElement += `
+  <article class="post-item">
+  <img class="post-item__thumbnail" src="${resto.pictureId}"
+    alt="${resto.name}">
+  <div class="post-item__content">
+    <p class="post-item__date">${resto.city} -<a href="#" class="post-item__date__author">${resto.rating}</a>
+    </p>
+    <h1 class="post-item__title"><a href="#">${resto.name}</a></h1>
+    <p class="post-item__description">${resto.description}</p>
+    <div class="footer">
+      <p class="post-item__description">Contact:</p>
+      <div class="row">
+        <a href="#"><i class="fa fa-facebook"></i></a>
+        <a href="#"><i class="fa fa-instagram"></i></a>
+        <a href="#"><i class="fa fa-youtube"></i></a>
+        <a href="#"><i class="fa fa-twitter"></i></a>
+      </div>
+    </div>
+  </div>
+</article>
+  `;
+});
+
+document.querySelector('.posts').innerHTML = restaurantElement;
 
 document.addEventListener('keyup', function (e) {
   if (e.keyCode == 32 || e.keyCode == 13) {
